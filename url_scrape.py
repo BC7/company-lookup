1f263b65108eed55331e0acdb2e64f0fb0dd68bc
Brian Ng <brian.ngobidi@.com>
2017-07-11 19:23:22 -0400

    added argument to scan any given cvs (given the template is the same

--- url_scrape.py
+++ url_scrape.py
@@ -1,6 +1,7 @@
 import scrapy
 import result_item
 import csvmanager
+import sys
 
 from csvmanager import CsvManager
 from scrapy.selector import Selector
@@ -10,51 +11,111 @@ class CompanySearchSpider(scrapy.Spider):
     #spider process
 
     # Base URL for a google search
-    BASE_URL="https://www.google.com/search?sclient=psy-ab&hl=en&site=webhp&source=hp&btnG=Search&q="
+    BASE_GOOGLE_URL="https://www.google.com/search?sclient=psy-ab&hl=en&site=webhp&source=hp&btnG=Search&q="
     name = 'companysearch'
-    allowed_domains = ["google.com"]
+
     start_urls = []
     download_delay = 2
 
+    manager = CsvManager(sys.argv[1])
+    orgData = manager.getOrgs()
+    csvdata = [len(orgData)]
+
     def start_requests(self):
-        self.manager = CsvManager("smaller_dataset.csv")
-        self.orgData = self.manager.getOrgs()
         i = 1
-        for i in range(1, len(self.orgData)):
-            self.start_urls.append(self.BASE_URL + self.orgData[i][0].replace(' ', '+'))
+        for key in self.orgData.keys():
+            self.start_urls.append(self.BASE_GOOGLE_URL + key.replace(' ', '+'))
             i += 1
 
+        i = 0
         for url in self.start_urls:
-            meta = {}
-            meta['data'] = self.orgData[self.start_urls.index(url)]
-            yield scrapy.Request(url=url, callback=self.parse, meta=meta)
+            # for data in self.orgData[self.start_urls.index(url)]:
+            # self.csvdata[self.start_urls.index(url)] = self.orgData[self.start_urls.index(url)]
+                # i += 1
+            yield scrapy.Request(url=url, dont_filter=True, callback=self.results_parse)
 
 
-    def parse(self, response):
-        # Selector and xpath based on google search results
+    def results_parse(self, response):
+        # Selector and xpaths based on google search results
         # Will need to modify to support crawling multiple search engines (If that is required later during development)
-        scanMarker = 0
+        searchedOrg = {}
+        searchedOrg[0] = response.url[len(self.BASE_GOOGLE_URL):].replace("+", " ")
         potentialresources = Selector(response).xpath('//div[@class="g"]')
         for i in range (0, 6):
-            meta = {}
             link = potentialresources[i]
             linkextract = link.xpath('//h3[@class="r"]/a/@href').extract()[i]
             item = StackItem()
             item['title'] = link.xpath('//h3[@class="r"]/a/text()').extract()[i]
             item['url'] = linkextract[7:linkextract.find("&sa=U&ved")]
 
-            # daisy chaining meta information to scan for on the found links
-            meta['data'] = response.meta['data']
-            yield scrapy.Request(url=item['url'], callback=self.scanLinks, meta=meta)
+
+            yield scrapy.Request(url=item['url'], callback=self.scanLinks, meta=searchedOrg, dont_filter=True)
 
     def scanLinks(self, response):
-        print(response.meta['data'][0][0]+"\n\n\n"+"wtffff")
-
-# Webcrawler process
-process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-})
+        # # print("\n\n\nTEST: " + str(response.meta[0]))
+        print("\n\nWorking on url: "+ response.url+"\nWith given meta for org: " + str(response.meta[0]))
 
 
-process.crawl(CompanySearchSpider)
-process.start() # the script will block here until the crawling is finished
+        # check if url is owned by the company (minimal check since vartions can happen in a domain)
+        if str(response.meta[0]).replace(" ", "") in str(response.url):
+            print("LINK IS USEFULLL\n\n\n")
+            self.addlink(str(response.meta[0]), str(response.url))
+
+
+        # check if page mentions a company email
+        if str(response.meta[0]).replace(" ", "") in str(response.body):
+            print("LINK IS USEFULLL\n\n\n")
+            self.addlink(str(response.meta[0]), str(response.url))
+
+        # checks if page mentions company in any way
+        if "Ltd" in response.meta[0]:
+            if str(response.meta[0]) in str(response.body):
+                print("LINK IS USEFULLL\n\n\n")
+                self.addlink(str(response.meta[0]), str(response.url))
+        elif "Limited" in response.meta[0]:
+            if str(response.meta[0].replace("Limited", "Ltd")) in str(response.body):
+                print("LINK IS USEFULLL\n\n\n")
+                self.addlink(str(response.meta[0]), str(response.url))
+        elif response.meta[0] in str(response.body):
+            print("LINK USEFULL\n\n\n")
+            self.addlink(str(response.meta[0]), str(response.url))
+
+        # checks if page mentions company address from adreess columns or city
+        if self.orgData[response.meta[0]][0] in str(response.body):
+            print("Link USEFULLLL\n\n")
+            self.addlink(str(response.meta[0]), str(response.url))
+        elif self.orgData[response.meta[0]][1] in str(response.body):
+            print("Link USEFULLLL\n\n")
+            self.addlink(str(response.meta[0]), str(response.url))
+        elif self.orgData[response.meta[0]][3] in str(response.body):
+            print("Link USEFULLLL\n\n")
+            self.addlink(str(response.meta[0]), str(response.url))
+
+        self.manager.updateCSV(self.orgData)
+
+    def addlink(self, key, url):
+        # check if cell isn't populated with a link already
+        # if it is, check the next available cell
+        if self.orgData[key][4] == "":
+            self.orgData[key][4] = str(url)
+        elif self.orgData[key][5] == "":
+            self.orgData[key][5] = str(url)
+        elif self.orgData[key][6] == "":
+            self.orgData[key][6] = str(url)
+        # elif self.orgData[key][7] == "":
+        #     self.orgData[key][7] = str(url)
+
+        print("Url added\n" + str(self.orgData[key]))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('Error: Please specify path to csv file..')
+        sys.exit()
+    else:
+        # Webcrawler process
+        process = CrawlerProcess({
+            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+        })
+        process.crawl(CompanySearchSpider)
+        process.start() # the script will block here until the crawling is finished
