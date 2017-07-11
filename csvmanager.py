790b2dcd8f55560168295cbcd26d356f00a509c1
Brian Ng <brian.ngobidi@.com>
2017-07-11 19:22:09 -0400

    fixed reading/writting error for out of index column (I was populating too many useful links

--- csvmanager.py
+++ csvmanager.py
@@ -21,14 +21,17 @@ class CsvManager(object):
             # will add the header again when updated the file
             # iterating over keys in url_scrape without the header
             self.file_header = res.pop("Company Name")
-            return res
+
+        csvfile.close()
+        return res
 
     def updateCSV(self, data):
         with open(self.file, 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|')
 
             # re-add header as the first row
-            csvswriter.writerow([self.file_header[0],self.file_header[1],self.file_header[2],self.file_header[3],self.file_header[4],self.file_header[5],self.file_header[6],self.file_header[7],self.file_header[8]])
+            csvwriter.writerow([self.file_header[0],self.file_header[1],self.file_header[2],self.file_header[3],self.file_header[4],self.file_header[5],self.file_header[6]])#,self.file_header[7],self.file_header[8]])
             for key in data.keys():
                 print(str(key) + "\n")
-                csvwriter.writerow([key , str(data[key][0]) , data[key][1] , data[key][2] , data[key][3] , data[key][4] , data[key][5] , data[key][6] , data[key][7] ])
+                csvwriter.writerow([key , str(data[key][0]) , data[key][1] , data[key][2] , data[key][3] , data[key][4] , data[key][5] , data[key][6] ]) # , data[key][7] ])
+        csvfile.close()
